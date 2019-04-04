a4d385075f0712147c4331419988d20b46c7d600
Esteban Alvarado <estalvgs1999@gmail.com>
2019-04-04 13:50:10 -0600

    (DES) + SERIALIZACION DE JSON POR ID [IMPLEMENTACION]
    ------------------------------------------------------------------------------
    1. Monté un switch para la serialización y deserialización de JSON del jugador.
    2. Cada id tiene un tipo de mensaje asociado:
    -id:1 confirmar alfo
    -id:2 enviar la palabra al servidor
    -id:3 pass/ jugar de nuevo
    -id:4 expert_request
    ------------------------------------------------------------------------------
    *Creado 04/04/2019
    
    Curso Algoritmos y Estructuras de Datos II [TEC CR]
    
    @estalvgs1999 @ErickCR12 @OscarAraya18

--- ServerProject/JSON/PlayerMessage.cpp
+++ ServerProject/JSON/PlayerMessage.cpp
@@ -14,6 +14,12 @@ PlayerMessage::PlayerMessage(){
     word = "";
 };
 
+
+void PlayerMessage::setMessage1_(int id,bool answer) {
+    this-> id = id,
+    this-> answer = answer;
+}
+
 void PlayerMessage::setMessage2_(int id,string word, int fRow,int fCol,bool is_vertical) {
     this-> id = id,
     this-> word = word;
@@ -22,16 +28,33 @@ void PlayerMessage::setMessage2_(int id,string word, int fRow,int fCol,bool is_v
     this-> isVertical = is_vertical;
 }
 
+void PlayerMessage::setMessage3_(int id,bool pass) {
+    this-> id = id,
+    this-> pass = pass;
+}
+
+void PlayerMessage::setMessage4_(int id,bool exp,string word) {
+    this-> id = id,
+    this-> exp_request = exp;
+    this-> word = word;
+}
+
 /* -------------------------------------------
  *              SERIALIZATION
  * -------------------------------------------*/
 
 template <typename Writer>
-void PlayerMessage::Serializer1_(Writer &writer) const{}
+void PlayerMessage::Serializer1_(Writer &writer) const{
+    writer.StartObject();
+    writer.String("id");
+    writer.Int(id);
+    writer.String("answer");
+    writer.Bool(answer);
+    writer.EndObject();
+}
 
 template <typename Writer>
 void PlayerMessage::Serializer2_(Writer &writer) const {
-
     writer.StartObject();
     writer.String("id");
     writer.Int(id);
@@ -47,6 +70,31 @@ void PlayerMessage::Serializer2_(Writer &writer) const {
 
 }
 
+template <typename Writer>
+void PlayerMessage::Serializer3_(Writer &writer) const{
+
+    writer.StartObject();
+    writer.String("id");
+    writer.Int(id);
+    writer.String("pass");
+    writer.Bool(pass);
+    writer.EndObject();
+}
+
+template <typename Writer>
+void PlayerMessage::Serializer4_(Writer &writer) const{
+    writer.StartObject();
+    writer.String("id");
+    writer.Int(id);
+    writer.String("exp_request");
+    writer.Bool(exp_request);
+    writer.String("word");
+    writer.String(word.c_str());
+    writer.EndObject();
+}
+
+//------------------------------------------------------
+
 string PlayerMessage::serialize() {
     StringBuffer sB;
     Writer<StringBuffer> writer(sB);
@@ -56,6 +104,12 @@ string PlayerMessage::serialize() {
             break;
         case 2:
             Serializer2_(writer);
+            break;
+        case 3:
+            Serializer3_(writer);
+            break;
+        case 4:
+            Serializer4_(writer);
             break;
         default:
             cout<<">> NO HAY NINGUNA COINCIDENCIA"<<endl;
@@ -75,12 +129,34 @@ string PlayerMessage::serialize() {
         doc.Parse(json);
 
         int id = doc["id"].GetInt();
-        string pWord = doc["word"].GetString();
-        int pFirstRow = doc["firstRow"].GetInt();
-        int pFirstCol = doc["firstCol"].GetInt();
-        bool pVertical = doc["is_Vertical"].GetBool();
 
-        parsedPlayer->setMessage2_(id,pWord,pFirstRow,pFirstCol,pVertical);
+        switch (id){
+            case 1: {
+                bool answer = doc["answer"].GetBool();
+                parsedPlayer->setMessage1_(id, answer);
+                break;
+            }
+            case 2: {
+                string pWord = doc["word"].GetString();
+                int pFirstRow = doc["firstRow"].GetInt();
+                int pFirstCol = doc["firstCol"].GetInt();
+                bool pVertical = doc["is_Vertical"].GetBool();
+
+                parsedPlayer->setMessage2_(id, pWord, pFirstRow, pFirstCol, pVertical);
+                break;
+            }
+            case 3: {
+                
+                break;
+            }
+            case 4:{
+
+                break;
+            }
+            default:
+                return nullptr;
+        }
+
         return parsedPlayer;
     }
 
--- ServerProject/JSON/PlayerMessage.hpp
+++ ServerProject/JSON/PlayerMessage.hpp
@@ -17,22 +17,44 @@ class PlayerMessage {
 
 private:
     int id; // Indica el tipo de mensaje que se va a serializar
+
     string word;
     int firstRow;
     int firstCol;
     bool isVertical;
 
+    bool answer;
+    bool pass;
+    bool exp_request;
+
+
     //! @brief Method responsible for serializing the object to JSON
+    //! @note CONFIRM
     //! \tparam Writer
     //! \param writer
     template<typename Writer>
     void Serializer1_(Writer &writer) const;
 
     //! @brief Method responsible for serializing the object to JSON
+    //! @note SEND WORD
     //! \tparam Writer
     //! \param writer
     template<typename Writer>
     void Serializer2_(Writer &writer) const;
+
+    //! @brief Method responsible for serializing the object to JSON
+    //! @note PASS/PLAY_AGAIN
+    //! \tparam Writer
+    //! \param writer
+    template<typename Writer>
+    void Serializer3_(Writer &writer) const;
+
+    //! @brief Method responsible for serializing the object to JSON
+    //! @note EXPERT_REQUEST
+    //! \tparam Writer
+    //! \param writer
+    template<typename Writer>
+    void Serializer4_(Writer &writer) const;
 
 public:
 
@@ -44,11 +66,27 @@ public:
      * -------------------------------------------*/
 
     //! @brief Configure message attributes
+    //! \param [in]id : message identifier
+    //! \param [in]answer :  true/false
+    void setMessage1_(int id,bool answer);
+
+    //! @brief Configure message attributes
+    //! \param [in]id : message identifier
     //! @param [in]word : Player word
     //! @param [in]fRow : Row of the first LetterTile
     //! @param [in]fCol : Column of the first LetterTile
-    void setMessage1_(int id,string word,int fRow,int fCol,bool vertical);
     void setMessage2_(int id,string word,int fRow,int fCol,bool vertical);
+
+    //! @brief Configure message attributes
+    //! \param [in]id : message identifier
+    //! \param [in]pass : true if playar wanna pass his turn/false if player wants replay his movements.
+    void setMessage3_(int id,bool pass);
+
+    //! @brief Configure message attributes
+    //! \param [in]id : message identifier
+    //! @param [in]exp_request: boolean that indicates if player wants to use expert validation
+    //! @param [in]word : Player word
+    void setMessage4_(int id,bool exp_request,string word);
 
     //! @brief Serializer Interface
     //! @return [out] :JSON of the object
