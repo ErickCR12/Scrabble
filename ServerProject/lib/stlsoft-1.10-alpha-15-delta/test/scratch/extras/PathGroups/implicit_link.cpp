99658d3508f816ade3b6ddf317c5f0cdeb5093ae
Esteban Alvarado <estalvgs1999@gmail.com>
2019-04-07 18:39:46 -0600

    IMPLEMENTACION DE TODOS LOS MENSAJES DEL CLIENTE EN EL SERVIDOR

--- ServerProject/lib/stlsoft-1.10-alpha-15-delta/test/scratch/extras/PathGroups/implicit_link.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <fastformat/implicit_link.h>
-
-#include <pantheios/implicit_link/core.h>
-#include <pantheios/implicit_link/fe.simple.h>
-#include <pantheios/implicit_link/be.WindowsDebugger.h>
-
